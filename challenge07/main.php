68ba5d798cc28f0e5d0aeb7260ccd585ebf46d2d
jlorente <jose.lorente.martin@gmail.com>
2013-10-28 17:16:58 +0100

    Challenge 07 optimized

--- challenge07/main.php
+++ challenge07/main.php
@@ -10,124 +10,149 @@
  * 
  * @author Jose Lorente Martin
  */
-$stdin = fopen('php://stdin', 'r');
+Graph::main();
 
-$nodeList = array();
-while (($passWordChars = fgets($stdin)) !== false) {
-    $passWordChars = str_split(trim($passWordChars));
+class Graph
+{
+    protected $previousNext = array();
     
-    $beforeList = array();
-    if (isset($nodeList[$passWordChars[0]])) {
-        $beforeList[0] = $nodeList[$passWordChars[0]];
-    } else {
-        $beforeList[0] = new Node($passWordChars[0]);
-        $nodeList[$passWordChars[0]] = $beforeList[0];
+    protected $nextPrevious = array();
+    
+    protected $vertexes = array();
+    
+    public $queue;
+    
+    public function __construct()
+    {
+        $this->queue = new SplQueue();
     }
     
-    for ($i = 1, $t = count($passWordChars); $i < $t; ++$i) {
-        if (isset($nodeList[$passWordChars[$i]])) {
-            $node = $nodeList[$passWordChars[$i]];
-        } else {
-            $node = new Node($passWordChars[$i]);
-            $nodeList[$passWordChars[$i]] = $node;
+    public static function main()
+    {
+        $graph = new Graph();
+        $graph->createFromStdin();
+        $graph->resolve();
+    }
+    
+    public function createFromStdin()
+    {
+        $stdin = fopen('php://stdin', 'r');
+
+        while (($passWordChars = fgets($stdin)) !== false) {
+            $passWordChars = str_split(trim($passWordChars));
+            
+            $this->vertexes[$passWordChars[0]] = $passWordChars[0];
+            $prev = 0;
+            for ($i = 1, $t = count($passWordChars); $i < $t; ++$i) {
+                $this->previousNext[$passWordChars[$i - 1]][$passWordChars[$i]] = true;
+                $this->nextPrevious[$passWordChars[$i]][$passWordChars[$i - 1]] = true;
+                $this->vertexes[$passWordChars[$i]] = $passWordChars[$i];
+            }
+        }
+    }
+    
+    public function resolve()
+    {
+        $orderBlocks = array();
+        $this->queue = new SplQueue();
+        foreach ($this->vertexes as $vertex) {
+            if (!isset($this->nextPrevious[$vertex])) {
+                $this->queue->enqueue($vertex);
+                $block[] = $vertex;
+            }
+        }
+        $orderBlocks[] = $block;
+        
+        while ($this->queue->isEmpty() !== true) {
+            $vertexPivot = $this->queue->dequeue();
+
+            if (isset($this->previousNext[$vertexPivot])) {
+                $previousNext = $this->previousNext[$vertexPivot];
+                $block = array();
+                foreach ($previousNext as $next => $v) {
+                    unset($this->previousNext[$vertexPivot][$next]);
+                    unset($this->nextPrevious[$next][$vertexPivot]);
+                    
+                    if (count($this->nextPrevious[$next]) <= 0) {
+                        $this->queue->enqueue($next);
+                        $block[] = $next;
+                    }
+                }
+                
+                if (count($block) > 0) {
+                    $orderBlocks[] = $block;
+                }
+            }
+        }
+        unset($block);
+        
+        $results = array();
+        $results[0] = '';
+        for ($i = 0, $l = count($orderBlocks); $i < $l; ++$i) {
+            $perm = new Permutation($orderBlocks[$i]);
+            $solutions = $perm->getSolutions();
+            $aux = array();
+            for ($j = 0, $m = count($solutions); $j < $m; ++$j) {
+                for ($k = 0, $n = count($results); $k < $n; ++$k) {
+                    $aux[] = $results[$k].implode($solutions[$j]);
+                }
+            }
+            $results = $aux;
         }
         
-        foreach ($beforeList as $before) {
-            $before->before($node);
-            $node->after($before);
+        for ($i = 0, $l = count($results); $i < $l; ++$i) {
+            $pivot = $i;
+            $value = $results[$pivot];
+            
+            while ($pivot > 0 && $results[$pivot - 1] > $value) {
+                $results[$pivot] = $results[$pivot - 1];
+                $pivot--;
+            }
+            $results[$pivot] = $value;
         }
-        $beforeList[] = $node;
+        
+        for ($i = 0; $i < $l; ++$i) {
+            echo $results[$i].PHP_EOL;
+        }
     }
 }
 
-$values = array_values($nodeList);
-$options = false;
-for ($i = 0, $t = count($values); $i < $t; ++$i) {
-    $pivot = $i;
-    $value = $values[$i];
-    while ($pivot > 0 && ($check = Node::goesAfter($values[$pivot - 1], $value)) >= 0) {
-        if ($check == 0) {
-            $options = true;
-        }
-        $values[$pivot] = $values[$pivot - 1];
-        $pivot--;
-    }
-    $values[$pivot] = $value;
-}
-
-Node::printNodes($values);
-
-
-class Node
+class Permutation
 {
-    protected $before = array();
+    protected $solutions;
     
-    protected $after = array();
+    protected $elements;
     
-    protected $value;
-     
-    public function __construct($value)
+    public function __construct(array $elements)
     {
-        $this->value = $value;
+        $this->elements = $elements;
     }
-
-    public function before(Node $before, $deep = true)
+    
+    protected function resolvePermutations(array $array, array $solution = array())
     {
-        $this->before[$before->getValue()] = $before;
-
-        if ($deep === true) {
-            $before->after($this, false);
-            foreach ($before->getBeforeList() as $beforeDeep) {
-                $this->before($beforeDeep);
-            }
+        $l = count($array);
+        if ($l <= 0) {
+            $this->solutions[] = $solution;
         }
-    }
-
-    public function after(Node $after, $deep = true)
-    {
-        $this->after[$after->getValue()] = $after;
-
-        if ($deep === true) {
-            $after->before($this, false);
-            foreach ($after->getAfterList() as $afterDeep) {
-                $this->after($afterDeep);
-            }
+        
+        foreach ($array as $key => $element) {
+            $copyArray = $array;
+            $copySolution = $solution;
+            
+            $copySolution[] = $element;
+            unset($copyArray[$key]);
+            
+            $this->resolvePermutations($copyArray, $copySolution);
         }
     }
     
-    public function getBeforeList()
+    public function getSolutions()
     {
-        return $this->before;
-    }
-    
-    public function getAfterList()
-    {
-        return $this->after;
-    }
-    
-    public function getValue()
-    {
-        return $this->value;
-    }
-    
-    public static function goesAfter(Node $a, Node $b)
-    {
-        $aAfterList = $a->getAfterList();
-        $aBeforeList = $a->getBeforeList();
-        if (isset($aAfterList[$b->getValue()])) {
-            return 1;
-        } elseif (!isset($aAfterList[$b->getValue()]) && !isset($aBeforeList[$b->getValue()])) {
-            return 0;
-        } else {
-            return -1;
+        if ($this->solutions === null) {
+            $this->solutions = array();
+            $this->resolvePermutations($this->elements);
         }
-    }
-    
-    public static function printNodes(array $nodes)
-    {
-        foreach ($nodes as $node) {
-            echo $node->getValue();
-        }
-        echo PHP_EOL;
+        
+        return $this->solutions;
     }
 }
